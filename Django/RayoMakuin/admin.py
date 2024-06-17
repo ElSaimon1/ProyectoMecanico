881f764412dd25d185a0a21898c4aeaacfaf9cfb
ElSaimon1 <143034636+ElSaimon1@users.noreply.github.com>
2024-06-17 14:27:53 -0400

    django 2.0
    2.0

--- Django/RayoMakuin/admin.py
+++ Django/RayoMakuin/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Tipo_servicio, Formulario
 
 # Register your models here.
+
+admin.site.register(Tipo_servicio)
+admin.site.register(Formulario)
--- Django/RayoMakuin/models.py
+++ Django/RayoMakuin/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
 # Create your models here.
+
+class Tipo_servicio(models.Model):
+    id_tipo_ser = models.AutoField(db_column='id_tipo_ser', primary_key=True)
+    Servicio = models.CharField(max_length=20, blank=False, null=False)
+    
+    def __str__(self): 
+        return str(self.Servicio)
+
+class comuna(models.Model):
+    id_comuna = models.AutoField(db_column='id_comuna', primary_key=True)
+    comuna = models.CharField(max_length=20, blank=False, null=False)
+    
+    def __str__(self): 
+        return str(self.comuna)
+
+class Formulario(models.Model):
+    patente             = models.CharField(primary_key=True, max_length=6)
+    marca               = models.CharField(max_length=20)
+    modelo              = models.CharField(max_length=20)
+    telefono            = models.CharField(max_length=45)
+    id_tipo_ser         = models.ForeignKey('Tipo_servicio',on_delete=models.CASCADE, db_column='id_tipo_ser')
+    comuna              = models.CharField(max_length=45)
+    Detalles_servicio   = models.CharField(max_length=20) 
+    Ingrese_archivo     = models.CharField(max_length=20)  
+
+
